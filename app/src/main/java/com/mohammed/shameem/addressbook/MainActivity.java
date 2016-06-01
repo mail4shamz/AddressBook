1fe517a3c449f522ef7e32e6ec160ae2583aed5b
mohammedshameem <mr.mohammedshameem@gmail.com>
2016-06-01 23:35:13 +0530

    Changes

--- app/src/main/java/com/mohammed/shameem/addressbook/MainActivity.java
+++ app/src/main/java/com/mohammed/shameem/addressbook/MainActivity.java
@@ -25,6 +25,7 @@ DBTools dbToolsObject=new DBTools(this);
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //
             }
         });
     }
