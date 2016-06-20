5067267eca931e7940d8a815fcdef19a2c41079d
mohammedshameem <mr.mohammedshameem@gmail.com>
2016-06-20 19:53:24 +0530

    Design and Color scheme changes

--- app/src/main/java/com/mohammed/shameem/addressbook/adapter/AddressListAdapter.java
+++ app/src/main/java/com/mohammed/shameem/addressbook/adapter/AddressListAdapter.java
@@ -2,9 +2,11 @@ package com.mohammed.shameem.addressbook.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mohammed.shameem.addressbook.R;
 import com.mohammed.shameem.addressbook.holder.SingleAddressDetailHolder;
 
 import java.util.ArrayList;
@@ -13,16 +15,22 @@ import java.util.ArrayList;
  * Created by shameem on 6/19/2016.
  */
 public class AddressListAdapter extends RecyclerView.Adapter<AddressListAdapter.AddressListViewHolder> {
+    Context context;
+    ArrayList<SingleAddressDetailHolder> singleAddressDetailHolders;
+    LayoutInflater inflater;
+
     public AddressListAdapter(Context context, ArrayList<SingleAddressDetailHolder> singleAddressDetailHolders) {
         this.context = context;
         this.singleAddressDetailHolders = singleAddressDetailHolders;
+        inflater = LayoutInflater.from(this.context);
     }
 
-    Context context;
-ArrayList<SingleAddressDetailHolder> singleAddressDetailHolders;
+
     @Override
     public AddressListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View cardView = inflater.inflate(R.layout.contact_entries, parent, false);
+        AddressListViewHolder addressListViewHolder=new AddressListViewHolder(cardView);
+        return addressListViewHolder;
     }
 
     @Override
