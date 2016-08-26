c71cb8dc798705b10283414cec8011dbf14018a6
mohammedshameem <shameem@xminds.in>
2016-08-26 18:02:18 +0530

    Changed

--- app/src/main/java/com/mohammed/shameem/addressbook/view/MainActivity.java
+++ app/src/main/java/com/mohammed/shameem/addressbook/view/MainActivity.java
@@ -1,60 +1,59 @@
 package com.mohammed.shameem.addressbook.view;
 
+import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mohammed.shameem.addressbook.adapter.AddressListAdapter;
 import com.mohammed.shameem.addressbook.controller.DBTools;
 import com.mohammed.shameem.addressbook.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SearchView.OnQueryTextListener {
+    private Toolbar toolbar;
     private Intent mainActivityIntent;
     private TextView tvContactsId;
     private DBTools dbToolsObject;
+    private String searchQuery;
     private RecyclerView mRecyclerView;
     private LinearLayoutManager linearLayoutManager;
     private AddressListAdapter addressListAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        mainActivityIntent = getIntent();
+        if (Intent.ACTION_SEARCH.equalsIgnoreCase(mainActivityIntent.getAction())) {
+            searchQuery=mainActivityIntent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(MainActivity.this, "The searched Name is "+searchQuery, Toast.LENGTH_LONG).show();
+        }
 
 
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddContact.class);
-                startActivity(intent);
-              /*  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-*/
-            }
-        });
     }
 
     private void initViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
         dbToolsObject = new DBTools(this);
         mRecyclerView = (RecyclerView) findViewById(R.id.card_recycler_view);
-        linearLayoutManager = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,
-                false);
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(linearLayoutManager);
-        addressListAdapter=new AddressListAdapter(MainActivity.this);
+        addressListAdapter = new AddressListAdapter(MainActivity.this);
         mRecyclerView.setAdapter(addressListAdapter);
     }
 
@@ -65,6 +64,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                startActivity(new Intent(MainActivity.this, AddContact.class));
+                break;
+        }
 //Just checking
     }
 
@@ -72,6 +76,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
+        /*final MenuItem item = menu.findItem(R.id.menu_search);*/
+        SearchView searchView= (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        SearchManager searchManager= (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
@@ -86,5 +94,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
